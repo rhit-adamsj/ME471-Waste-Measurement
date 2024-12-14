63ce247c7eaa42de83ff1a2578e2f4a62db5b3a2
rhit-adamsj <adamsj@rose-hulman.edu>
2024-12-13 20:06:22 -0500

    Update AutonomousWasteMeasurement.ino

--- Programs/AutonomousWasteMeasurement/AutonomousWasteMeasurement.ino
+++ Programs/AutonomousWasteMeasurement/AutonomousWasteMeasurement.ino
@@ -5,11 +5,13 @@ DS3231 myRTC;
 
 void setup() {
   // put your setup code here, to run once:
-  myRTC.setClockMode(false);
-  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  while (myRTC.getMinute()) {
+
+    
+  }
 }
