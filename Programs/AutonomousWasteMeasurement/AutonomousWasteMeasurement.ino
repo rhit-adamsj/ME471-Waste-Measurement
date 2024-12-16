266c4b25c7847d7d7e959d5bdaded9e7ab4c3804
rhit-adamsj <adamsj@rose-hulman.edu>
2024-12-16 17:52:49 -0500

    Update AutonomousWasteMeasurement.ino
    Added preliminary load cell readings

--- Programs/AutonomousWasteMeasurement/AutonomousWasteMeasurement.ino
+++ Programs/AutonomousWasteMeasurement/AutonomousWasteMeasurement.ino
@@ -1,17 +1,29 @@
 #include <DS3231.h>
 #include <Wire.h>
 
+int cellOne; cellTwo; cellThree; cellFour; totalLoad;
+
 DS3231 myRTC;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  while (myRTC.getMinute()) {
+  cellOne = calculateLoad(digitalRead(6));
+  cellTwo = calculateLoad(digitalRead(7));
+  cellThree = calculateLoad(digitalRead(8));
+  cellFour = calculateLoad(digitalRead(9));
+  totalLoad = cellOne + cellTwo + cellThree + cellFour;
+}
 
-    
-  }
+int calculateLoad() {
+  int load;
+
+
+
+  return load;
 }
