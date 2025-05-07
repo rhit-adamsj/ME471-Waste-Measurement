402497d075cdd0a1ed4b9456114820bb6bbf79d4
rhit-adamsj <adamsj@rose-hulman.edu>
2025-05-07 10:53:58 -0400

    Fixed button interrupts and improved night mode logic

--- Programs/AutonomousWasteMeasurement/AutonomousWasteMeasurement.ino
+++ Programs/AutonomousWasteMeasurement/AutonomousWasteMeasurement.ino
@@ -14,6 +14,7 @@ float load4;
 float currentLoad; float maxLoad; 
 float prevLoad;
 bool shouldTare = false;
+bool forceTare = false;
 
 volatile uint8_t mainEventFlags = 0;
 #define ALARM_WAKEUP_FLAG 0x01
@@ -51,13 +52,14 @@ HX711 cell4;
 #define LOADCELL4_SCK_PIN 27
 #define LOADCELL4_DOUT_PIN 33
 
-#define WAKE_PIN 19
+#define WAKE_PIN 2
 #define BACKLIGHT_CONTROL_PIN 10
 #define ALARM_LENGTH 1  // Minutes
 #define MORNING_HOUR 8  // Time at which device will wake up after shutting down overnight
 #define NIGHT_HOUR 18   // Time at which device will shut down for the night
-#define TARE_BUTTON_PIN 2
-#define LCD_BUTTON_PIN 3
+#define PUSHBUTTON_TARE_PIN 19
+#define PUSHBUTTON_LCD_PIN 18
+#define DEBOUNCE_DELAY 20
 
 LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
 
@@ -132,8 +134,8 @@ void setup() {
   pinMode(LOADCELL2_DOUT_PIN, INPUT);
   pinMode(LOADCELL3_DOUT_PIN, INPUT);
   pinMode(LOADCELL4_DOUT_PIN, INPUT);
-  pinMode(TARE_BUTTON_PIN, INPUT_PULLUP);
-  pinMode(LCD_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(PUSHBUTTON_TARE_PIN, INPUT_PULLUP);
+  pinMode(PUSHBUTTON_LCD_PIN, INPUT_PULLUP);
 
   digitalWrite(BACKLIGHT_CONTROL_PIN, HIGH);
 
@@ -179,6 +181,7 @@ void setup() {
 }
 
 void loop() {
+
   load1 = calculateLoad(cell1);
   cell1.power_down();
   load2 = calculateLoad(cell2);
@@ -188,10 +191,10 @@ void loop() {
   load4 = calculateLoad(cell4);
   cell4.power_down();
 
-  Serial.print("Cell 1 = "); Serial.println(load1);
-  Serial.print("Cell 2 = "); Serial.println(load2);
-  Serial.print("Cell 3 = "); Serial.println(load3);
-  Serial.print("Cell 4 = "); Serial.println(load4);
+  // Serial.print("Cell 1 = "); Serial.println(load1);
+  // Serial.print("Cell 2 = "); Serial.println(load2);
+  // Serial.print("Cell 3 = "); Serial.println(load3);
+  // Serial.print("Cell 4 = "); Serial.println(load4);
 
   getRTCValues();
   currentLoad = load1+load2+load3+load4;
@@ -203,34 +206,19 @@ void loop() {
     // Very negative weight means dumpster must currently be off the scale
     // Display last measured load. --> Do not update the value or re-tare.
     Serial.println("Dumpster is not on scale!");
-    if (lcdEnabled) {
-      digitalWrite(BACKLIGHT_CONTROL_PIN, HIGH);
-      updateLCD(prevLoad);
-    } else {
-      digitalWrite(BACKLIGHT_CONTROL_PIN, LOW);
-    }
+    updateLCD(prevLoad);
     shouldTare = false;
     // waitToTare = false; Useless?
   } else if (prevLoad > 20 && (currentLoad-prevLoad)/prevLoad < -0.5) {
     // If relative change in weight shows more than a 50% decrease from last measurement and is beyond threshold where noise could reasonably be the culprit,
     // Dumpster must have been unloaded and set back down on scale. --> Re-tare the scale and record the last measured load
     Serial.println("Dumpster has been emptied!");
-    if (lcdEnabled) {
-      digitalWrite(BACKLIGHT_CONTROL_PIN, HIGH);
-      updateLCD(prevLoad);
-    } else {
-      digitalWrite(BACKLIGHT_CONTROL_PIN, LOW);
-    }
+    updateLCD(prevLoad);
     prevLoad = currentLoad;
     shouldTare = true;
     // waitToTare = false;
   } else {
-    if (lcdEnabled) {
-      digitalWrite(BACKLIGHT_CONTROL_PIN, HIGH);
-      updateLCD(currentLoad);
-    } else {
-      digitalWrite(BACKLIGHT_CONTROL_PIN, LOW);
-    }
+    updateLCD(currentLoad);
     shouldTare = false;
     prevLoad = currentLoad;
   }
@@ -241,33 +229,55 @@ void loop() {
   Serial.println();
   Serial.print("Total load = "); Serial.print(currentLoad); Serial.println(" lbs");
   
-  // Powerdown until needed again
-  if (hour >= NIGHT_HOUR) {
+  // Night Mode
+  while (hour >= NIGHT_HOUR || hour < MORNING_HOUR) { 
     digitalWrite(BACKLIGHT_CONTROL_PIN, LOW);
     alarmHour = MORNING_HOUR;
     alarmMinute = 0;
     alarmSecond = 0;
     alarmBits = 0b00001000; // Alarm when hour, minute, and second match, i.e. on the hour specified
-    goToSleep();
-    digitalWrite(BACKLIGHT_CONTROL_PIN, HIGH);
-  } else {
-    alarmHour = 0;
-    alarmMinute = (minute + ALARM_LENGTH) % 60;
-    alarmSecond = second;
-    alarmBits = 0b00001100; // Alarm when minutes and seconds match, i.e. on the minute specified
-    goToSleep();
+    goToSleep(); 
   }
+
+  // Powerdown until needed again
+  alarmHour = 0;
+  alarmMinute = (minute + ALARM_LENGTH) % 60;
+  alarmSecond = second;
+  alarmBits = 0b00001100; // Alarm when minutes and seconds match, i.e. on the minute specified
+  goToSleep();
+
   // delay(1000);
+
+  // Check for LCD button pressed
+  if (mainEventFlags & PUSHBUTTON_LCD_FLAG) {
+    delay(DEBOUNCE_DELAY);
+    if (!digitalRead(PUSHBUTTON_LCD_PIN))
+    {
+      digitalWrite(BACKLIGHT_CONTROL_PIN, !(digitalRead(BACKLIGHT_CONTROL_PIN)));
+    }
+    mainEventFlags &= !PUSHBUTTON_LCD_FLAG;
+  }
 
   cell1.power_up();
   cell2.power_up();
   cell3.power_up();
   cell4.power_up();
-  if (shouldTare) {
+
+  if (mainEventFlags & PUSHBUTTON_TARE_FLAG) {   // Check for tare button pressed
+    delay(DEBOUNCE_DELAY);
+    if (!digitalRead(PUSHBUTTON_TARE_PIN))
+    {
+      lcd.clear();
+      cell1.tare(); cell2.tare(); cell3.tare(); cell4.tare();
+      maxLoad = 0;
+      tempRef = tempC;
+      shouldTare = false;
+    }
+    mainEventFlags &= !PUSHBUTTON_TARE_FLAG;
+  } else if (shouldTare) {  // Check for automated tare
     cell1.tare(); cell2.tare(); cell3.tare(); cell4.tare();
     tempRef = tempC;
     shouldTare = false;
-    forceTare = false;
   }
 }
 
@@ -350,13 +360,12 @@ void goToSleep() {
   sleep_enable();
 
   noInterrupts();
-  attachInterrupt(digitalPinToInterrupt(WAKE_PIN), SleepISR, FALLING);  // Assign parameter values for Alarm 1
-  EIFR = _BV(INTF2); // Clear interrupt flag for wakePin interrupt
-  attachInterrupt(digitalPinToInterrupt(TARE_BUTTON_PIN), TareISR, FALLING);
-  EIFR = _BV(INTF4); // Clear interrupt flag for wakePin interrupt
-  attachInterrupt(digitalPinToInterrupt(LCD_BUTTON_PIN), lcdISR, FALLING);
-  EIFR = _BV(INTF5); // Clear interrupt flag for wakePin interrupt
-
+  attachInterrupt(digitalPinToInterrupt(WAKE_PIN), SleepISR, LOW);  // Assign parameter values for Alarm 1
+  EIFR |= _BV(digitalPinToInterrupt(WAKE_PIN)); // Clear interrupt flag for WAKE_PIN interrupt
+  attachInterrupt(digitalPinToInterrupt(PUSHBUTTON_TARE_PIN), TareISR, FALLING);
+  EIFR |= _BV(digitalPinToInterrupt(PUSHBUTTON_TARE_PIN)); // Clear interrupt flag for PUSHBUTTON_TARE_PIN interrupt
+  attachInterrupt(digitalPinToInterrupt(PUSHBUTTON_LCD_PIN), lcdISR, FALLING);
+  EIFR |= _BV(digitalPinToInterrupt(PUSHBUTTON_LCD_PIN)); // Clear interrupt flag for PUSHBUTTON_LCD_PIN interrupt
   interrupts();
   sleep_cpu();
 
@@ -366,16 +375,22 @@ void goToSleep() {
 void SleepISR() {
   // Disable interrupt pin to prevent continuous interruptions
   detachInterrupt(digitalPinToInterrupt(WAKE_PIN));
+  detachInterrupt(digitalPinToInterrupt(PUSHBUTTON_TARE_PIN));
+  detachInterrupt(digitalPinToInterrupt(PUSHBUTTON_LCD_PIN));
 }
 
 void TareISR() {
-  detachInterrupt(digitalPinToInterrupt(TARE_BUTTON_PIN));
-  forceTare = true;
-  Serial.println(forceTare);
+  detachInterrupt(digitalPinToInterrupt(WAKE_PIN));
+  detachInterrupt(digitalPinToInterrupt(PUSHBUTTON_TARE_PIN));
+  detachInterrupt(digitalPinToInterrupt(PUSHBUTTON_LCD_PIN));
+  Serial.println("FORCE TARE");
+  mainEventFlags |= PUSHBUTTON_TARE_FLAG;
 }
 
 void lcdISR() {
-  detachInterrupt(digitalPinToInterrupt(LCD_BUTTON_PIN));
-  lcdEnabled = !lcdEnabled;
-  Serial.println(lcdEnabled);
+  detachInterrupt(digitalPinToInterrupt(WAKE_PIN));
+  detachInterrupt(digitalPinToInterrupt(PUSHBUTTON_TARE_PIN));
+  detachInterrupt(digitalPinToInterrupt(PUSHBUTTON_LCD_PIN));
+  Serial.println("TOGGLE LCD");
+  mainEventFlags |= PUSHBUTTON_LCD_FLAG;
 }
